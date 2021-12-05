e1c415b115ea077f9dcb80e95c379409735903eb
kisungyou <kisungyou@outlook.com>
2021-12-05 15:19:32 -0500

    natural reduction

---
+++ R/reduction_2022Y.R
@@ -0,0 +1,13 @@
+#' GMM Reduction by You (2022)
+#' 
+#' 
+#' 
+#' 
+#' @concept 
+#' @concept reduction
+#' @export
+gmr2022Y <- function(gmm, merge=c("moment","wasserstein")){
+  ## INPUTS
+  check_gmmobj(gmm, "gmr2022Y")
+  
+}
